8321d34da6c1415b623a56ba72e8c2e0cfd97100
Eremey Ikashev <hezeri546@gmail.com>
2022-05-13 13:40:35 +0700

    Added vectors of shape parameters

--- src/geometry/main.cpp
+++ src/geometry/main.cpp
@@ -1,32 +1,38 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
-#include <geometry_lib/check_figure.hpp>
-#include <geometry_lib/print_circle.hpp>
-#include <geometry_lib/print_triangle.hpp>
+#include "../geometry_lib/check_figure.hpp"
+#include "../geometry_lib/get_circle_params.hpp"
+#include "../geometry_lib/get_triangle_params.hpp"
+#include "../geometry_lib/print_circle.hpp"
+#include "../geometry_lib/print_triangle.hpp"
 
 using namespace std;
 
 int main()
 {
-    int line_number = 1;
+    int curent_line = 1;
     string line;
     ifstream file("./input.txt");
+    vector<vector<double>> circles, triangles;
+
     if (file.is_open()) {
         while (getline(file, line)) {
             string figure = check_figure(line);
             if (figure == "circle") {
-                print_circle(line);
+                circles.push_back(get_circle_params(line));
             } else if (figure == "triangle") {
-                print_triangle(line);
+                triangles.push_back(get_triangle_params(line));
             } else {
-                cout << "Syntax error on the line [" << line_number << "]"
+                cout << "Syntax error on the line [" << curent_line << "]"
                      << endl;
             }
-            line_number++;
+            curent_line++;
         }
-        string output = "";
+        print_circle(circles);
+        print_triangle(triangles);
 
         return 0;
     }
