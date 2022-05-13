ea4b32a66b456080a019febf86c7c0647f57b7c1
Eremey Ikashev <hezeri546@gmail.com>
2022-05-13 13:35:37 +0700

    Add functions for calculating circle parameters

---
+++ src/geometry_lib/circle_area.cpp
@@ -0,0 +1,6 @@
+#include "../geometry_lib/circle_area.hpp"
+#include <math.h>
+
+double circle_area(double radius){
+    return M_PI * radius * radius;
+}
---
+++ src/geometry_lib/circle_perimeter.cpp
@@ -0,0 +1,6 @@
+#include "../geometry_lib/circle_perimeter.hpp"
+#include <math.h>
+
+double circle_perimeter(double radius){
+    return 2.0 * M_PI * radius;
+}
